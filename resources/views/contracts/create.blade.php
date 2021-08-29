e91c59c26bee91a10a73e41c664210d1406a4928
Mohammed Fahad <H4ck3r@Mohammeds-MacBook-Pro.local>
2021-08-29 14:29:09 +0300

    wip on sales app

--- resources/views/contracts/create.blade.php
+++ resources/views/contracts/create.blade.php
@@ -9,12 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
                     <form method="POST" action="{{ route('saveContract') }}">
                         @csrf
                         <!-- Contract Number -->
                         <div class="mt-4">
-                          <div class="flex flex-row ">
+                          <div class="flex flex-col">
                             <!-- <input class="mr-3 block rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
                             type="number" min="2017"  step="1" value="2021" name="year" required /> -->
                             <x-input id="contract_number" class="block mt-1 w-full" type="text" name="contract_number" placeholder="Contract Number" :value="old('contract_number')"  />
