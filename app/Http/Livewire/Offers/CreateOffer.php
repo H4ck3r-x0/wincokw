45f26a24a88269f1494150ff31a06e08bb31432e
Mohammed Fahad <H4ck3r@Mohammeds-MacBook-Pro.local>
2021-09-14 15:18:17 +0300

    wip

--- app/Http/Livewire/Offers/CreateOffer.php
+++ app/Http/Livewire/Offers/CreateOffer.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Livewire\Offers;
 
+use Carbon\Carbon;
 use Livewire\Component;
 use App\Models\Client;
 
 class CreateOffer extends Component
 {
     public $clients = [];
+    public $offerExperationDate = null;
 
     public function mount()
     {
         $this->clients = Client::all();
+        $this->offerExperationDate = Carbon::now()->addDays(15)->format('Y-m-d');
     }
 
     public function render()
--- app/Http/Livewire/Offers/OffersIndex.php
+++ app/Http/Livewire/Offers/OffersIndex.php
@@ -6,8 +6,6 @@ use Livewire\Component;
 
 class OffersIndex extends Component
 {
-    public $show = false;
-
     public function render()
     {
         return view('livewire.offers.offers-index');
